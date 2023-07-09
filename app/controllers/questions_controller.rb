250e17c70d291770465174505580acfaa5877de3
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-09 21:04:51 +0900

    firstcommit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @answer = Answer.new
+    @answers = @question.answers
+  end
+
+  def create
+    Question.create(question_params)
+    redirect_to action: :index
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content, :name)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'questions#index'
+  resources :questions, only: [:index,:show, :new, :create]do
+   resources :answers, only: :create
+  end
+end
